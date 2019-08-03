e33a107386daa0dadc6ab7615863540a9342e95b
dmitryisemenov <dmitriysemenov1994th@gmail.com>
2019-08-03 12:41:10 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -2,20 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-console.log('lol')
-
 class App extends React.Component {
-    componentDidMount() {
-        fetch(`https://pokeapi.co/api/v2/pokemon/1/`)
-        .then(data => data.json())
-        .then(data => console.log(data.name, data.sprites.front_default))
+    constructor () {
+        super()
+        this.state = {
+            name: "",
+            img: ""
+        }
+    }
+
+    getPokemon = async () => {
+        const takeFromAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 807)}/`)
+        const randomPokemon = takeFromAPI.json();
+        return randomPokemon;
+    }
+
+    componentDidMount = async () => {
+        const pokemon = await this.getPokemon().then(data => {return data})
+        console.log(pokemon)
+        this.setState({
+            name: pokemon.name,
+            img: pokemon.sprites.front_default
+        })
     }
 
     render() {
+        const {name, img} = this.state
+
         return (
-            <div>
-                {/* RENDER YOUR POKEMON HERE */}
-            </div>
+           <div className='pokemon'>
+               <img src={img} alt="pokemon"></img>
+                <div className='pokemon__name'>
+                    <p>{name}</p>
+                </div>
+           </div>
         );
     }
 }
